79099ac08b2fb3d076f88bce6c354704cc117fba
nitin awari <nitinawari0116@gmail.com>
2024-10-18 15:58:49 +0530

    github cloud storage

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -5,12 +5,12 @@ export const usernameValidation = z
   .min(2, 'Username must be at least 2 characters')
   .max(20, 'Username must be no more than 20 characters')
   .regex(/^[a-zA-Z0-9_]+$/, 'Username must not contain special characters');
-
+  
 export const signUpSchema = z.object({
   username: usernameValidation,
 
   email: z.string().email({ message: 'Invalid email address' }),
-  password: z
+  password: z 
     .string()
     .min(6, { message: 'Password must be at least 6 characters' }),
 });
